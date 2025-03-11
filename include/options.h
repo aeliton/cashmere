6731e054331fa6a9325709414aa2c2cfe055329f
Aeliton G. Silva <aeliton@aeliton.com>
2025-03-11 12:23:45 -0300

    Initial commit
    Signed-off-by: Aeliton G. Silva <aeliton@aeliton.com>

---
+++ include/options.h
@@ -0,0 +1,37 @@
+// Cashmere - a distributed conflict free replicated database.
+// Copyright (C) 2025 Aeliton G. Silva
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#ifndef BARK_OPTIONS_BARK
+#define BARK_OPTIONS_BARK
+
+#include <string>
+#include <unordered_map>
+
+using OptionsMap = std::unordered_map<std::string, std::string>;
+
+class Options {
+public:
+  Options(int argc, const char *const *const argv);
+
+  bool contains(const std::string &option) const;
+  size_t size() const;
+
+  static OptionsMap parse(int argc, const char *const *const argv);
+
+private:
+  OptionsMap _options;
+};
+
+#endif
---
+++ src/options.cpp
@@ -0,0 +1,36 @@
+// Cashmere - a distributed conflict free replicated database.
+// Copyright (C) 2025 Aeliton G. Silva
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include "options.h"
+
+Options::Options(int argc, const char *const *const argv)
+  : _options()
+{
+}
+
+bool Options::contains(const std::string &option) const
+{
+  return _options.find(option) != _options.end();
+}
+
+size_t Options::size() const
+{
+  return _options.size();
+}
+
+OptionsMap Options::parse(int argc, const char *const *const argv)
+{
+  return {};
+}
