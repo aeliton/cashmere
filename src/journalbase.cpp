c546dd3f9921f14e9259fc687f75679449d8001a
Aeliton G. Silva <aeliton@aeliton.com>
2025-08-05 12:26:46 -0300

    Move random.{cpp,h} to utils
    Signed-off-by: Aeliton G. Silva <aeliton@aeliton.com>

--- src/journalbase.cpp
+++ src/journalbase.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "journalbase.h"
-#include "random.h"
+#include "utils/random.h"
 #include <cassert>
 
 namespace Cashmere
