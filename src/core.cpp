1680f58ee967c17feb8d934c6d8465690e805a36
Aeliton G. Silva <aeliton@aeliton.com>
2026-01-24 08:46:19 -0300

    Remove unused functions
    Signed-off-by: Aeliton G. Silva <aeliton@aeliton.com>

--- src/core.cpp
+++ src/core.cpp
@@ -17,21 +17,12 @@ BrokerStore::BrokerStore()
   _builders["cache"] = &Journal::create;
 }
 
-BrokerBasePtr BrokerStore::get([[maybe_unused]] Id id) {
-  return nullptr;
-}
-
-bool BrokerStore::load([[maybe_unused]] const std::string& path) {
-  return {};
-}
-
 BrokerStorePtr BrokerStore::instance() {
   if (!_instance) {
     _instance = std::shared_ptr<BrokerStore>(new BrokerStore());
   }
   return _instance;
 }
-
 
 BrokerBasePtr BrokerStore::build(const std::string& url) const
 {
